26ab646a7c21111778b7ff455ae5d80c7cf774e8
Md Aatif Raza <mdaatifraza5556@gmail.com>
2025-06-17 09:11:10 +0530

    data and time

---
+++ Day_11.py
@@ -0,0 +1,32 @@
+# date and times
+
+import datetime
+from traceback import print_tb
+
+date = datetime.date(2025, 1, 2)
+print(date)
+
+today = datetime.date.today()
+print(today)
+
+time = datetime.time(12, 13, 25)
+print(time)
+
+now = datetime.datetime.now()
+now = now.strftime("%H:%M:%S %d/%m/%Y")
+print(now)
+
+target_datetime = datetime.datetime(2022, 1, 2, 12, 13, 20)
+current_datetime = datetime.datetime.now()
+
+if target_datetime < current_datetime:
+    print(f'Target date has passed')
+else:
+    print(f'Target date has not passed')
+
+#---------------🎯 Challenge---------------
+#---------------- Calculate the days between two dates---------------
+
+#diff = abs((current_datetime - target_datetime).days))
+diff = (current_datetime - target_datetime).days
+print(diff)
